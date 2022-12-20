6fa8586cd24d724f6ee55345d390cd7b7dff1e0e
Owen <owen@coloradomacs.com>
2022-12-20 01:46:44 -0600

    audio in

--- js/app.js
+++ js/app.js
@@ -119,7 +119,7 @@ function loadRNBOScript(version) {
 }
 
 const handleSuccess = (stream) => {
-    const source = context.createMediaStreamSource(stream);
+    const source = createMediaStreamSource(stream);
     source.connect(device.node);
 }
 navigator.mediaDevices.getUserMedia({ audio: true, video: false })
